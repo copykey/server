41cb35561d9824fe2aa79e9b9b1046930c4d487e
Kevin Black <kevin3.black@gmail.com>
2018-10-21 11:49:49 -0500

    commit

--- app.py
+++ app.py
@@ -1,9 +1,10 @@
-from flask import Flask, request, send_file, redirect, url_for, send_from_directory
+from flask import Flask, request, send_file, redirect, url_for, send_from_directory, render_template
 import os
 import time
 from copykey import copykey
-from threading import Thread
 from werkzeug.utils import secure_filename
+import cv2
+from base64 import b64encode
 
 app = Flask(__name__)
 
@@ -19,24 +20,29 @@ def allowed_file(filename):
 
 @app.route('/upload', methods=['POST'])
 def upload_file():
-    try:
-        # check if the post request has the file part
-        if 'file' not in request.files:
-            return 'No file part'
-        file = request.files['file']
-        type = request.form['type']
-        # if user does not select file, browser also
-        # submit a empty part without filename
-        if file.filename == '':
-            return 'No selected file'
-        if file and allowed_file(file.filename):
-            filename = secure_filename(file.filename)
-            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-            copykey.copykey(os.path.join(app.config['UPLOAD_FOLDER'], filename), os.path.join(app.config['UPLOAD_FOLDER'], filename + '.scad'), type)
-            time.sleep(10)
-        return redirect(url_for('get_file', filename=filename + '.scad'))
-    except:
-        return 'Failure'
+    # check if the post request has the file part
+    if 'file' not in request.files:
+        return 'No file part'
+    file = request.files['file']
+    type = request.form['type']
+    # if user does not select file, browser also
+    # submit a empty part without filename
+    if file.filename == '':
+        return 'No file selected'
+    if file and allowed_file(file.filename):
+        filename = secure_filename(file.filename)
+        file.save(os.path.join(app.config['UPLOAD_FOLDER'], 'video.mp4'))
+        img1, img2 = copykey.copykey(
+            os.path.join(app.config['UPLOAD_FOLDER'], 'video.mp4'),
+            os.path.join(app.config['UPLOAD_FOLDER'], filename + '.scad'),
+            type,
+            cool_video_output=os.path.join(app.config['UPLOAD_FOLDER'], 'cool_video.mp4')
+        )
+        return render_template("index.html", result=True, filename=".".join(filename.split(".")[:-1]) + '.scad', img1=b64encode(cv2.imencode('.jpg', img1)[1]).decode('utf-8'), img2=b64encode(cv2.imencode('.jpg', img2)[1]).decode('utf-8'))
+        cv2.imshow(img1)
+        cv2.waitKey(0)
+    # return redirect(url_for('get_file', filename=filename + '.scad'))
+    return render_template("index.html")
 
 @app.route('/get/<filename>')
 def get_file(filename):
@@ -45,5 +51,16 @@ def get_file(filename):
 
 @app.route('/')
 def index():
-    with open("index.html", "r") as f:
-        return f.read()
+    return render_template("index.html")
+
+@app.after_request
+def add_header(r):
+    """
+    Add headers to both force latest IE rendering engine or Chrome Frame,
+    and also to cache the rendered page for 10 minutes.
+    """
+    r.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
+    r.headers["Pragma"] = "no-cache"
+    r.headers["Expires"] = "0"
+    r.headers['Cache-Control'] = 'public, max-age=0'
+    return r
